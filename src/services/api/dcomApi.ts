6b5c6a2131219a73d4e8851a1276b2e1f941d7e0
Malith <malit.tilak@gmail.com>
2023-01-27 11:43:20 +0530

    Fix Rehydration Issue

--- src/services/api/dcomApi.ts
+++ src/services/api/dcomApi.ts
@@ -1,6 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { REHYDRATE } from 'redux-persist';
-
 import { ResponseDTO, getShopFromShopDTO } from './apiTypes';
 
 export const dcomApi = createApi({
@@ -8,11 +6,6 @@ export const dcomApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://dcom-native-interview.s3.amazonaws.com/api/merchant/query/',
   }),
-  extractRehydrationInfo(action, { reducerPath }) {
-    if (action.type === REHYDRATE) {
-      return action.payload[reducerPath];
-    }
-  },
   endpoints: builder => ({
     fetchMerchants: builder.query<any, string>({
       query: address => address,
